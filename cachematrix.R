63139a49af7aa130ead32f60832e49383699b5a5
Holler <gholler@MacBook-Pro-de-Guillaume.local>
2017-04-30 13:32:36 +0200

    initial release

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function returns an object that stores a matrix and its inverse
+## matrix. The inverse matrix is initially null.
+## hence this object is a cache for the inverse matrix.
+##
+## Parameter 'M' is the matrix that should be cached
+## Returns a list object that containts accessor functions  for the cached matrix
+## ( $get/set_matrix) and its inverse ($get/set_inverse_matrix)
+##
+makeCacheMatrix <- function(M = matrix()) {
+    
+    M_1 <- NULL
+    setMatrix <- function(N) {
+        M <<- N
+        M_1 <<- NULL
+    }
+    getMatrix <- function() M
+    setInverseMatrix <- function(inverse) M_1 <<- inverse
+    getInverseMatrix <- function() M_1
+    list(setMatrix = setMatrix, getMatrix = getMatrix,
+         setInverseMatrix = setInverseMatrix,
+         getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function returns the inverse matrix of a given cache matrix, caching the result
+## for successive calls with the same cached matrix. 
+##
+## parameter 'cacheMatrix' is the cached matrix to inverse (built from a previous call to makeCache)
+## Returns a matrix that is the inverse of M
+cacheSolve <- function(cacheMatrix, ...) {
+     M_1 <- cacheMatrix$getInverseMatrix()
+     if (is.null(M_1)){
+         M_1 <- solve(M, ...) 
+         cacheMatrix$setInverseMatrix(M_1)
+     }
+     else {
+         message("cached result")
+     }
+     M_1
 }
